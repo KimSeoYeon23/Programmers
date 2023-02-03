295008e4d0168972e7043d145739ff78e82c1730
김서연 <115128505+KimSeoYeon23@users.noreply.github.com>
2023-02-03 18:37:14 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,14 @@
+function solution(array, n) {
+    var answer = 0;
+    var result = 0;
+    for(var arr of array) {
+        if(arr == n) {
+            result++;
+        } else {
+            0;
+        }
+        answer = result;
+        
+    }
+    return answer;
+}
