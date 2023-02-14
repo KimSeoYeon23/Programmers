3918c81ee66ffc9b481d32aba7e4f7409ba3e961
김서연 <115128505+KimSeoYeon23@users.noreply.github.com>
2023-02-14 14:24:15 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,9 +2,8 @@ function solution(age) {
     var answer = '';
     var array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
     var string = age.toString();
-    var strArr = string.split('');
     for(var i=0; i<string.length; i++) {
-        answer += array[parseInt(strArr[i])];
+        answer += array[parseInt(string[i])];
     }
     return answer;
 }
