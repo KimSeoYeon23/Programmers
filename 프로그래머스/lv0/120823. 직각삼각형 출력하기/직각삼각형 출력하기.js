c221cada24f2cc02817b464ebba6550a5475bbe7
김서연 <115128505+KimSeoYeon23@users.noreply.github.com>
2023-02-14 14:10:41 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 59.42 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -9,13 +9,11 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    var n = input[0];
-    var array = [,,,];
-    for(var i=0; i<n; i++) {
-        if(i !== n) {
-            array[n] += '*'
-        }
-        console.log(array[n].split('undefined').join(''))
-    }
-    
+    solution(Number(input[0]));
 });
+
+function solution(n) {
+    for(let i = 1; i < n + 1; i++) {
+        console.log('*'.repeat(i));
+    }
+}
