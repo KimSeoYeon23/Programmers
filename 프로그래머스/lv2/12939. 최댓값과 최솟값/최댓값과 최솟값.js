bb6a3fb87a0d1c41e350a6d6351f6f5fc60eaa91
김서연 <115128505+KimSeoYeon23@users.noreply.github.com>
2023-02-23 13:36:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,6 @@
 function solution(s) {
     var answer = '';
     let nums = s.split(' ');
-    let newArr = nums.map((num) => {
-        return Number(num)
-    }).sort((a, b) => a - b);
-    answer = `${newArr[0]} ${newArr[newArr.length - 1]}`
-    return answer;
+
+    return Math.min(...nums) + ' ' + Math.max(...nums);
 }
