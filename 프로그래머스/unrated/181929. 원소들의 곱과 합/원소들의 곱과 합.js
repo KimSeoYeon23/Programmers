9a3a189ad9c9e3019a07562aa8a05cbf72f15b17
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-06-14 13:01:20 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let multiply = 1;
+    let sum = 0;
+    
+    for(let i=0; i<num_list.length; i++) {
+        multiply *= num_list[i];
+        sum += num_list[i];
+    }
+    
+    return multiply < Math.pow(sum, 2) ? 1 : 0
+}
