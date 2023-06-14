a18d58ce3eb4878d3bbbd40455daf97febf42e1f
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-06-14 13:04:51 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let a = '';
+    let b = '';
+    for(num of num_list) {
+        if(num % 2 !== 0) {
+            a += num.toString();
+        } else {
+            b += num.toString();
+        }
+    }
+    return Number(a) + Number(b);
+}
