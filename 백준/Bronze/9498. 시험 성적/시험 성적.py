c606f49d347d3b569821da05eb1b8339310c87ea
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-09-10 16:06:50 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -2,11 +2,11 @@ score = int(input())
 
 if score >= 90:
   print('A')
-elif (score >= 80) and (score <= 89):
+elif (score >= 80):
   print('B')
-elif (score >= 70) and (score <= 79):
+elif (score >= 70):
   print('C')
-elif (score >= 60) and (score <= 69):
+elif (score >= 60):
   print('D')
 else:
   print('F')
