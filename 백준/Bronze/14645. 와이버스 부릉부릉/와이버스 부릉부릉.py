d694cb76c8ad1836d5d1c03526a561973b10bb17
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-09-21 18:45:25 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.py
@@ -0,0 +1,9 @@
+# n = 출발역과 종학역을 제외한 정거장의 수
+# k = 출발역에서 탑승하는 사람의 수
+n, k = map(int, input().split())
+
+# i번째 정거장에서 탑승하는 인원 a와 b
+for i in range(n):
+  a, b = map(int, input().split())
+print("비와이")
+  
