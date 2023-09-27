4947e2307001121159ad0aa90b32b4eb82cc32da
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-09-27 23:33:39 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,14 +1,11 @@
 def solution(num_list):
     multiple = 1
-    sum_num = 0
-    square = 0
-    for i in range(len(num_list)):
-        multiple *= num_list[i]
-        sum_num += num_list[i]
-        square = pow(sum_num, 2)
-    if multiple < square:
+    
+    for i in num_list:
+        multiple *= i
+    
+    if multiple < sum(num_list) ** 2:
         return 1
     else:
         return 0
-    
     
