b0fcd5b60be8f4857c243a7c8cf27e57152dc9a4
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-10-06 21:45:32 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,9 @@
+pledge = ["Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "Never gonna stop"]
+N = int(input())
+
+for _ in range(N):
+  sentence = input().strip()
+  if sentence not in pledge:
+    print("Yes")
+    exit()
+print("No")
