95345baf2bb1dc924ea5199ac10bf6d33be372ae
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-10-17 21:19:22 +0900

    [Bronze IV] Title: 시험 점수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5596. 시험 점수/시험 점수.py
+++ 백준/Bronze/5596. 시험 점수/시험 점수.py
@@ -1,9 +1,4 @@
 manse = sum(map(int, input().split()))
 mingook = sum(map(int, input().split()))
 
-if manse > mingook:
-  print(manse)
-elif manse < mingook:
-  print(mingook)
-else:
-  print(mingook)
+print(max(manse, mingook))
