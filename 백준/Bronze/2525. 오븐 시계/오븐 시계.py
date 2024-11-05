10a728c97cc3493c0b31c5206b90a5280db72548
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2024-11-05 09:47:12 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,13 @@
-H, M = map(int, input().split())
-N = int(input())
+h, m = map(int, input().split())
+t = int(input()) 
 
-# 총 분을 계산한다.
-total_minutes = H * 60 + M + N
+h += t // 60
+m += t % 60
 
-# 총 분을 다시 시간과 분으로 변환한다.
-new_H = (total_minutes // 60) % 24  # 24시간을 넘기면 0으로 리셋해야 하므로 % 24를 사용
+if m >= 60:
+    h += 1
+    m -= 60
+if h >= 24:
+    h -= 24
 
-new_M = total_minutes % 60
-
-print(new_H, new_M)
+print(h, m)
