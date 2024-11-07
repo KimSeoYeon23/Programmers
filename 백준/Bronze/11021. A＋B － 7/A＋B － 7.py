9d7f5efc203ff384f9f8544b0b95dc981b0451c4
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2024-11-07 09:40:32 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,7 +1,4 @@
-import sys
-
-t = int(input())
-
-for i in range(t):
-  a, b = map(int, sys.stdin.readline().split())
-  print("Case #%d: %d" % (i+1, a+b))
+T = int(input())
+for i in range(1, T + 1):
+    a, b = map(int, input().split())
+    print(f'Case #{i}: {a+b}')
