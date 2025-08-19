689b3fd44255d2eed8d16835c6d188953472269b
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2025-08-19 14:12:31 +0900

    [level 0] Title: 배열 만들기 3, Time: 6.68 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,9 +1,6 @@
 function solution(arr, intervals) {
     var answer = [];
-    const inter1 = [...intervals[0]]
-    const inter2 = [...intervals[1]]
-    answer.push(arr.slice(inter1[0], inter1[1]+1))
-    answer.push(arr.slice(inter2[0], inter2[1]+1))
+    const [[a,b],[c,d]] = intervals;    
     
-    return answer.flat();
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
 }
